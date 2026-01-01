80119501d985b33619f14566dcacec920c54223a
SCW App Developer <ahmed@birba.om>
2026-01-01 20:10:39 +0400

    heading name change

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Navigation Test - SCW</title>
+    <title>Home page</title>
     <link rel="icon" type="image/webp" href="/storage/nav-img/scw-logo.webp">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
